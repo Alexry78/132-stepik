4e9cf1d685c951f4e5e8b04dce3500e6d9d11fca
Alexry78 <144147456+Alexry78@users.noreply.github.com>
2023-10-28 13:02:31 +0300

    test

---
+++ main(4).py
@@ -0,0 +1,14 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+a = input(" ")
+b = input(" ")
+c = input(" ")
+print(a)
+print(b)
+print(c)
